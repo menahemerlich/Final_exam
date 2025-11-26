d06c59a403a4f031712fccfd9c43fc8255da4374
menahem_erlich <erlichmenahem@gmail.com>
2025-11-26 17:29:42 +0200

    שלב א

--- api.py
+++ api.py
@@ -1,21 +1,31 @@
 from fastapi import FastAPI, UploadFile, File, Depends
-from sqlmodel import Session
-
+import sqlite3
 from functions.io_with_csv import read_csv
 from models.soldier import Soldier
-from db import create_db_and_tables, get_session
+from functions.create_table import soldiers_table, insert_soldiers, create_houses
+from inlay_strategy import inlay_strategy
 
 app = FastAPI()
-create_db_and_tables()
+DB_PATH = "soldiers.db"
+
+def get_connection():
+    return sqlite3.connect(DB_PATH)
 
 @app.post("/assignWithCsv")
-def assign_with_csv(file_name, session: Session = Depends(get_session)):
+def assign_with_csv(file_name):
+    conn = get_connection()
+    create_houses(conn, 2, 10)
+    soldiers_table(conn)
+
     data = read_csv(file_name)
     for row in data:
-        new_soldier = Soldier(row)
-        return new_soldier
-        session.add(new_soldier)
-        session.commit()
+        conn = get_connection()
+        new_soldier = Soldier(**row)
+        insert_soldiers(conn, new_soldier)
+    conn = get_connection()
+    inlay_strategy(conn)
+
+
 
 
 
--- models/soldier.py
+++ models/soldier.py
@@ -3,13 +3,12 @@ from typing import Optional
 
 class Soldier(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    personal_number: int
+    personal_number: str
     First_name: str
     Last_name: str
     sex: str
     City: str
-    Distance: int
-    status: str
+    Distance: str
 
 
 
